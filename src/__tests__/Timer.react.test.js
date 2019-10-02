4d0550a8e34ac7e2c8c1bd0f1d7991546a6f3154
Hagihara-A <knkr5456@gmail.com>
2019-10-03 08:24:08 +0900

    Timer's dispatch test #1

--- src/__tests__/Timer.react.test.js
+++ src/__tests__/Timer.react.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { act } from 'react-dom/test-utils'
 import Timer from '../components/Timer/Timer';
 
+jest.useFakeTimers()
 let container
 
 beforeEach(() => {
@@ -25,6 +26,127 @@ it('render Time', () => {
             , container)
     })
     const time = container.querySelector('span')
-    
+
     expect(time.textContent).toBe('INIT: 10')
+})
+
+it('confirm dispatch: INIT', () => {
+    const mapDispatchToProps = {
+        onReachTimeLimit: jest.fn(),
+        whenElapse: jest.fn(),
+        whenEnd: jest.fn()
+    }
+
+    act(() => {
+        let timerState = 'INIT'
+        ReactDOM.render(
+            <Timer
+                {...mapDispatchToProps}
+                timerState={timerState}
+                time={0}
+                timeLimit={3}
+            />, container
+        )
+    })
+    expect(mapDispatchToProps.onReachTimeLimit.mock.calls.length).toBe(0)
+    expect(mapDispatchToProps.whenElapse.mock.calls.length).toBe(0)
+    expect(mapDispatchToProps.whenEnd.mock.calls.length).toBe(0)
+
+
+})
+
+it('confirm dispatch : ELAPSE', () => {
+    const mapDispatchToProps = {
+        onReachTimeLimit: jest.fn(),
+        whenElapse: jest.fn(),
+        whenEnd: jest.fn()
+    }
+
+    act(() => {
+        let timerState = 'ELAPSE'
+        ReactDOM.render(
+            <Timer
+                {...mapDispatchToProps}
+                timerState={timerState}
+                time={0}
+                timeLimit={3}
+            />, container
+        )
+    })
+    jest.runOnlyPendingTimers()
+    expect(mapDispatchToProps.onReachTimeLimit.mock.calls.length).toBe(0)
+    expect(mapDispatchToProps.whenElapse.mock.calls.length).toBe(1)
+    expect(mapDispatchToProps.whenEnd.mock.calls.length).toBe(0)
+})
+
+it('confirm dispatch: ELAPSE & timeLimit', () => {
+    const mapDispatchToProps = {
+        onReachTimeLimit: jest.fn(),
+        whenElapse: jest.fn(),
+        whenEnd: jest.fn()
+    }
+
+    act(() => {
+        let timerState = 'ELAPSE'
+        ReactDOM.render(
+            <Timer
+                {...mapDispatchToProps}
+                timerState={timerState}
+                time={3}
+                timeLimit={3}
+            />, container
+        )
+    })
+    jest.runOnlyPendingTimers()
+    expect(mapDispatchToProps.onReachTimeLimit.mock.calls.length).toBe(1)
+    expect(mapDispatchToProps.whenElapse.mock.calls.length).toBe(0)
+    expect(mapDispatchToProps.whenEnd.mock.calls.length).toBe(0)
+})
+
+it('confirm dispatch: STOP', () => {
+    const mapDispatchToProps = {
+        onReachTimeLimit: jest.fn(),
+        whenElapse: jest.fn(),
+        whenEnd: jest.fn()
+    }
+
+    act(() => {
+        let timerState = 'STOP'
+        ReactDOM.render(
+            <Timer
+                {...mapDispatchToProps}
+                timerState={timerState}
+                time={0}
+                timeLimit={3}
+            />, container
+        )
+    })
+    jest.runOnlyPendingTimers()
+    expect(mapDispatchToProps.onReachTimeLimit.mock.calls.length).toBe(0)
+    expect(mapDispatchToProps.whenElapse.mock.calls.length).toBe(0)
+    expect(mapDispatchToProps.whenEnd.mock.calls.length).toBe(0)
+})
+
+it('confirm dispatch: END', () => {
+    const mapDispatchToProps = {
+        onReachTimeLimit: jest.fn(),
+        whenElapse: jest.fn(),
+        whenEnd: jest.fn()
+    }
+
+    act(() => {
+        let timerState = 'END'
+        ReactDOM.render(
+            <Timer
+                {...mapDispatchToProps}
+                timerState={timerState}
+                time={0}
+                timeLimit={3}
+            />, container
+        )
+    })
+    jest.runOnlyPendingTimers()
+    expect(mapDispatchToProps.onReachTimeLimit.mock.calls.length).toBe(0)
+    expect(mapDispatchToProps.whenElapse.mock.calls.length).toBe(0)
+    expect(mapDispatchToProps.whenEnd.mock.calls.length).toBe(1)
 })
