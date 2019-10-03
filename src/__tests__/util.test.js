7d4a72882cdc0130aecf34247852cef3f1e07577
Hagihara-A <knkr5456@gmail.com>
2019-10-03 22:16:22 +0900

    add test in case of no current timer

--- src/__tests__/util.test.js
+++ src/__tests__/util.test.js
@@ -21,7 +21,7 @@ test('get index from initState', () => {
         ]
     })
     const indexes = getCurrentTimerIndex(initState.get('timers'))
-    
+
     expect(indexes).toEqual(List([0]))
 })
 
@@ -46,4 +46,59 @@ test('get index from elapsed state', () => {
 
     const indexes = getCurrentTimerIndex(elapsedState.get('timers'))
     expect(indexes).toEqual(List([3, 1, 1]))
+})
+
+test('get index from  all-END timers', () => {
+    const timers = fromJS(
+        [
+            {
+                time: 3,
+                timerState: TS.END,
+                timeLimit: 3
+            },
+            [
+                {
+                    time: 2,
+                    timerState: TS.END,
+                    timeLimit: 2
+                },
+                {
+                    time: 2,
+                    timerState: TS.END,
+                    timeLimit: 2
+                },
+            ],
+            {
+                time: 5,
+                timerState: TS.END,
+                timeLimit: 5
+            },
+            [
+                {
+                    time: 2,
+                    timerState: TS.END,
+                    timeLimit: 2
+                },
+                {
+                    time: 2,
+                    timerState: TS.END,
+                    timeLimit: 2
+                },
+                [
+                    {
+                        time: 3,
+                        timerState: TS.END,
+                        timeLimit: 3
+                    },
+                    {
+                        time: 3,
+                        timerState: TS.END,
+                        timeLimit: 3
+                    },
+                ],
+            ],
+        ]
+    )
+    const idx = getCurrentTimerIndex(timers)
+    expect(idx).toBeNull()
 })
