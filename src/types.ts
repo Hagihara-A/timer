cda6e583a14b3ebeb4ec9c42563b6f1fe8a97784
Hagihara-A <knkr5456@gmail.com>
2019-12-22 00:32:09 +0900

    Remove immutablejs-oriented types.#21

--- src/types.ts
+++ src/types.ts
@@ -1,42 +1,11 @@
-import { List, Map } from "immutable";
-import { ItemId } from "@atlaskit/tree";
+import { TreeData, TreeItem } from "@atlaskit/tree";
 
-export interface TreeDataIm extends Map<string, any> {
-  rootId: ItemId;
-  items: Map<ItemId, TreeItemIm>;
-}
-
-interface data extends Map<string, number | string> {
-  timeLimit: number;
-  times: number;
-  power: number;
-  comment: string;
-}
-
-export interface TreeItemIm extends Map<string, any> {
-  id: ItemId;
-  children: List<ItemId>;
-  hasChildren?: boolean;
-  isExpanded?: boolean;
-  isChildrenLoading?: boolean;
-  data?: data;
-}
-
-export interface TimerItem {
-  time: number;
-  timerState: string;
-  timeLimit: number;
-}
-
-export interface Timers {
-  [index: number]: TimerItem | Timers;
-}
+export type Timers = TreeItem[];
 export interface Action {
   type: string;
   payload: any;
 }
-
-export interface State extends Map<string, any> {
-  timers: Map<string, any>;
-  tree: TreeDataIm;
+export interface State {
+  timers: Timers;
+  tree: TreeData;
 }
