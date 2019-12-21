beef34862187e0b7d8cf6435f7fa18175efcdfe4
Hagihara-A <knkr5456@gmail.com>
2019-12-21 19:59:44 +0900

    Updated initState.timers so that corresponding to tree

--- src/initState.js
+++ src/initState.js
@@ -1,7 +1,151 @@
 import { fromJS } from "immutable";
 
 export const initState = fromJS({
-  timers: [],
+  timers: [
+    {
+      id: "0",
+      children: [],
+      hasChildren: false,
+      isExpanded: false,
+      isChildrenLoading: false,
+      data: {
+        timeLimit: 3,
+        times: 1,
+        power: 130,
+        comment: ""
+      }
+    },
+    {
+      id: "1",
+      children: ["1-0", "1-1"],
+      hasChildren: true,
+      isExpanded: true,
+      isChildrenLoading: false,
+      data: {
+        timeLimit: 0,
+        times: 1,
+        power: 0,
+        comment: ""
+      }
+    },
+    {
+      id: "1-0",
+      children: [],
+      hasChildren: false,
+      isExpanded: false,
+      isChildrenLoading: false,
+      data: {
+        timeLimit: 2,
+        times: 1,
+        power: 260,
+        comment: ""
+      }
+    },
+    {
+      id: "1-1",
+      children: [],
+      hasChildren: false,
+      isExpanded: false,
+      isChildrenLoading: false,
+      data: {
+        timeLimit: 2,
+        times: 1,
+        power: 260,
+        comment: ""
+      }
+    },
+    {
+      id: "2",
+      children: [],
+      hasChildren: false,
+      isExpanded: false,
+      isChildrenLoading: false,
+      data: {
+        timeLimit: 5,
+        times: 1,
+        power: 260,
+        comment: ""
+      }
+    },
+    {
+      id: "3",
+      children: ["3-0", "3-1", "3-2"],
+      hasChildren: false,
+      isExpanded: true,
+      isChildrenLoading: false,
+      data: {
+        timeLimit: 0,
+        times: 1,
+        power: 0,
+        comment: ""
+      }
+    },
+    {
+      id: "3-0",
+      children: [],
+      hasChildren: false,
+      isExpanded: false,
+      isChildrenLoading: false,
+      data: {
+        timeLimit: 2,
+        times: 1,
+        power: 150,
+        comment: ""
+      }
+    },
+    {
+      id: "3-1",
+      children: [],
+      hasChildren: false,
+      isExpanded: false,
+      isChildrenLoading: false,
+      data: {
+        timeLimit: 2,
+        times: 1,
+        power: 150,
+        comment: ""
+      }
+    },
+    {
+      id: "3-2",
+      children: ["3-2-0", "3-2-1"],
+      hasChildren: true,
+      isExpanded: true,
+      isChildrenLoading: false,
+      data: {
+        timeLimit: 0,
+        times: 1,
+        power: 0,
+        comment: ""
+      }
+    },
+    {
+      id: "3-2-0",
+      children: [],
+      hasChildren: false,
+      isExpanded: false,
+      isChildrenLoading: false,
+      data: {
+        timeLimit: 3,
+        times: 1,
+        power: 150,
+        comment: ""
+      }
+    },
+    {
+      id: "3-2-1",
+      children: [],
+      hasChildren: false,
+      isExpanded: false,
+      isChildrenLoading: false,
+      data: {
+        timeLimit: 3,
+        times: 1,
+        power: 150,
+        comment: ""
+      }
+    }
+  ],
   tree: {
     rootId: "root",
     items: {
