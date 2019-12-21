1e4d83cb99b2e012d82cb185479e5920b0766e65
Hagihara-A <knkr5456@gmail.com>
2019-12-22 00:31:37 +0900

    use combineReducers instead of vanilla func and convert to ts.

---
+++ src/reducers/rootReducer.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import timersReducer from "./timersReducer";
+import treeReducer from "./treeReducer";
+
+export const rootReducer = combineReducers({
+  tree: treeReducer,
+  timers: timersReducer
+});
