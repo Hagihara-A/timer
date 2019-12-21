47419d4af0d650874d98024ad8cb3306294a3aa6
Hagihara-A <knkr5456@gmail.com>
2019-12-22 00:33:53 +0900

    Use immer instead of immutablejs

--- src/reducers/treeReducer.ts
+++ src/reducers/treeReducer.ts
@@ -1,209 +1,69 @@
+// TODO Remove following
+/* eslint-disable no-case-declarations */
 import {
   ItemId,
+  moveItemOnTree,
+  TreeData,
   TreeDestinationPosition,
+  TreeItem,
   TreeSourcePosition
 } from "@atlaskit/tree";
-import { fromJS, List, Map } from "immutable";
+import produce from "immer";
 import { actionTypes as AT } from "../actions";
 import { initState } from "../initState";
-import { Action, TreeDataIm, TreeItemIm } from "../types";
+import { Action } from "../types";
 import { getNewItemIds } from "../util";
-
-const initTreeItem = fromJS({
+const initTreeItem: TreeItem = {
+  id: undefined,
   children: [],
   hasChildren: false,
   isExpanded: false,
   isChildrenLoading: false
-});
+};
 export const setNewItemOnTree = (
-  tree: TreeDataIm,
+  tree: TreeData,
   parentId: ItemId,
   data = {}
 ) => {
-  const newId = getNewItemIds(tree, 1).get(0);
-  let newTree = tree.updateIn(["items", parentId, "children"], children =>
-    children.push(newId)
-  );
-
-  const newItem = initTreeItem.withMutations(item =>
-    item.set("id", newId).set("data", Map(data))
-  );
-  newTree = newTree.setIn(["items", newId], newItem);
+  const newId: ItemId = getNewItemIds(tree, 1)[0];
+  const newItem = produce(initTreeItem, draft => {
+    draft.id = newId;
+    draft.data = data;
+  });
+  const newTree = produce(tree, draft => {
+    draft.items[parentId].children.push(newId);
+    draft.items[newId] = newItem;
+  });
   return newTree;
 };
-const getItemIdFromPosition = (
-  tree: TreeDataIm,
-  position: TreeDestinationPosition
-): ItemId => {
-  if (typeof position.index === "undefined") return position.parentId;
-  else {
-    return tree.getIn(["items", position.parentId, "children", position.index]);
-  }
-};
-
-export const getParentItem = (
-  tree: TreeDataIm,
-  childId: ItemId
-): TreeItemIm => {
-  return tree.get("items").find((item: TreeItemIm) => {
-    return item.get("children").includes(childId);
-  });
-};
-export const getAllChildrenIds = (
-  tree: TreeDataIm,
-  parentItemId: ItemId
-): List<ItemId> => {
-  return tree
-    .getIn(["items", parentItemId, "children"])
-    .reduce((accum: List<ItemId>, id: ItemId) => {
-      if (tree.getIn(["items", id, "children"]).size > 0) {
-        return accum.concat(getAllChildrenIds(tree, id));
-      } else {
-        return accum.push(id);
-      }
-    }, List([parentItemId]));
-};
-
-export const removeItemFromTree = (
-  tree: TreeDataIm,
-  position: TreeSourcePosition
-): { tree: TreeDataIm; itemRemoved: ItemId } => {
-  const sourceParent = tree.getIn(["items", position.parentId]);
-  const newSourceChildren = sourceParent.get("children").delete(position.index);
-  const itemRemoved = getItemIdFromPosition(tree, position);
-  const newTree = tree.withMutations(tree =>
-    tree
-      .setIn(["items", position.parentId, "children"], newSourceChildren)
-      .setIn(
-        ["items", position.parentId, "hasChildren"],
-        newSourceChildren.size > 0
-      )
-      .setIn(
-        ["items", position.parentId, "isExpanded"],
-        newSourceChildren.size > 0 && sourceParent.get("isExpanded")
-      )
-  );
-
-  return {
-    tree: newTree,
-    itemRemoved
-  };
-};
-
-export const addItemToTree = (
-  tree: TreeDataIm,
-  position: TreeDestinationPosition,
-  item: ItemId
-): TreeDataIm => {
-  const destinationParent = tree.getIn(["items", position.parentId]);
-  if (typeof position.index === "undefined") {
-    if (destinationParent.get("children").size === 0) {
-      return tree.withMutations(tree => {
-        const itemToMutate = tree.getIn(["items", position.parentId]);
-        const newItem = itemToMutate
-          .update("children", children => children.push(item))
-          .set("hasChildren", true)
-          .set("isExpanded", true);
-        return tree.setIn(["items", position.parentId], newItem);
-      });
-    }
-  } else {
-    return tree.updateIn(
-      ["items", position.parentId, "children"],
-      (children: List<ItemId>) => children.insert(position.index, item)
-    );
-  }
-};
-const moveItemOnTree = (
-  tree: TreeDataIm,
-  from: TreeSourcePosition,
-  to: TreeDestinationPosition
-): TreeDataIm => {
-  const { tree: newTree, itemRemoved } = removeItemFromTree(tree, from);
-  return addItemToTree(newTree, to, itemRemoved);
-};
-
-const treeReducer = (
-  tree: TreeDataIm = initState.get("tree"),
-  action: Action
-) => {
+const treeReducer = (tree = initState.tree, action: Action) => {
   switch (action.type) {
-    case AT.SET_TREE:
-      return fromJS(action.payload.tree);
     case AT.ADD_TREE_ITEM: {
       const { parentId, timeLimit } = action.payload;
       return setNewItemOnTree(tree, parentId, { timeLimit });
     }
-    case AT.ADD_SECTION: {
-      const { parentId, title } = action.payload;
-      return setNewItemOnTree(tree, parentId, { title });
-    }
     case AT.REMOVE_ITEM: {
-      const { removeItemId } = action.payload;
-      const parentItem = tree
-        .get("items")
-        .find((value: TreeItemIm) =>
-          value.get("children").some((id: ItemId) => id === removeItemId)
-        );
-      const parentId = parentItem.get("id");
-      const removeItemIndex = parentItem.get("children").indexOf(removeItemId);
-      return tree
-        .deleteIn(["items", parentId, "children", removeItemIndex])
-        .deleteIn(["items", removeItemId]);
+      const { removeItemId }: { removeItemId: ItemId } = action.payload;
+      const parentItem = Object.values(tree.items).find(item =>
+        item.children.some(id => id === removeItemId)
+      );
+      const parentId = parentItem.id;
+      const removeItemIndex = parentItem.children.indexOf(removeItemId);
+      return produce(tree, draft => {
+        draft.items[parentId].children.splice(removeItemIndex, 1);
+        delete draft.items[removeItemId];
+      });
     }
     case AT.EDIT_ITEM: {
       const { editItemId, data } = action.payload;
-      return tree.updateIn(["items", editItemId, "data"], originData =>
-        originData.merge(data)
-      );
-    }
-    case AT.COPY_ITEM: {
-      const { originItemId } = action.payload;
-
-      const childrenIds = getAllChildrenIds(tree, originItemId);
-      const newItemIds = getNewItemIds(tree, childrenIds.size);
-      const correspond = Map(childrenIds.zip(newItemIds));
-
-      const newItems = childrenIds.reduce((accItems, curId) => {
-        const curItem: TreeItemIm = tree.getIn(["items", curId]);
-        const newId = correspond.get(curId);
-        const newChildren = curItem
-          .get("children")
-          .map((prevId: ItemId) => correspond.get(prevId));
-        const itemToAdd = curItem.set("children", newChildren).set("id", newId);
-
-        if (typeof newId === "string") {
-          return accItems.set(newId, itemToAdd);
-        } else {
-          return accItems;
-        }
-      }, Map({}));
-
-      const parentItem: TreeItemIm = tree
-        .get("items")
-        .find((item: TreeItemIm) =>
-          item.get("children").some((id: ItemId) => id === originItemId)
-        );
-      const indexToInsert =
-        parentItem
-          .get("children")
-          .findIndex((id: ItemId) => id === originItemId) + 1;
-      const newParentChildren = parentItem
-        .get("children")
-        .insert(indexToInsert, correspond.get(originItemId));
-
-      return tree
-        .update("items", items => items.merge(newItems))
-        .setIn(["items", parentItem.get("id"), "children"], newParentChildren);
+      return produce(tree, draft => {
+        const originalData = draft.items[editItemId].data;
+        draft.items[editItemId].data = { ...originalData, data };
+      });
     }
     case AT.ON_DRAG_END:
-      const {
-        source,
-        destination
-      }: {
-        source: TreeSourcePosition;
-        destination: TreeDestinationPosition;
-      } = action.payload;
+      const source: TreeSourcePosition = action.payload.source;
+      const destination: TreeDestinationPosition = action.payload.destination;
       if (!destination) return tree;
       return moveItemOnTree(tree, source, destination);
     case AT.TOGGLE_PROPERTY:
@@ -212,13 +72,12 @@ const treeReducer = (
         property
       }: {
         id: ItemId;
-        property: Pick<
-          TreeItemIm,
-          "hasChildren" | "isExpanded" | "isChildrenLoading"
-        >;
+        property: "hasChildren" | "isExpanded" | "isChildrenLoading";
       } = action.payload;
-      return tree.updateIn(["items", id, property], flag => !flag);
-
+      return produce(tree, draft => {
+        const flag = draft.items[id][property];
+        draft.items[id][property] = !flag;
+      });
     default:
       return tree;
   }
--- src/util.ts
+++ src/util.ts
@@ -1,133 +1,25 @@
 import {
   ItemId,
   mutateTree,
+  Path,
   TreeData,
   TreeDestinationPosition,
   TreeItem,
   TreeSourcePosition
 } from "@atlaskit/tree";
-import { List, Map } from "immutable";
 import { timerState } from "./reducers/timersReducer";
-import { TreeDataIm, Timers } from "./types";
 
-export function getCurrentTimerIndex(timers) {
-  for (let idx = 0; idx < timers.size; idx++) {
-    let elem = timers.get(idx);
-    if (List.isList(elem)) {
-      const childIndex = getCurrentTimerIndex(elem);
-      if (childIndex) {
-        return childIndex.unshift(idx);
-      }
-    } else {
-      const ts = elem.get("timerState");
-      if (
-        ts === timerState.INIT ||
-        ts === timerState.ELAPSE ||
-        ts === timerState.STOP
-      ) {
-        return List([idx]);
-      }
-    }
-  }
-  return null;
-}
-
-export const parseTreeData = (treeData: TreeData): Timers => {
-  const parseChild = (treeData: TreeData, item: TreeItem) => {
-    const childrenData = [];
-    if (item.children.length > 0) {
-      // eslint-disable-next-line no-unused-vars
-      for (const childId of item.children) {
-        const childItem = treeData.items[childId];
-        const childData = parseChild(treeData, childItem);
-        childrenData.push(childData);
-      }
-      return childrenData.filter(v => v); //remove undefined(in case of child doesn't have children nor timeLimit)
-    } else if (item.data.timeLimit) {
-      return {
-        time: 0,
-        timerState: "INIT",
-        timeLimit: item.data.timeLimit
-      };
-    }
-  };
-
-  const rootId = treeData.rootId;
-  const rootItem = treeData.items[rootId];
-
-  return parseChild(treeData, rootItem);
+export type TreeItemData = any;
+export type FlattenedItem = {
+  item: TreeItem;
+  path: Path;
 };
-export const getNewItemIds = (tree: TreeData | TreeDataIm, numIds: number) => {
-  const startNum = Map.isMap(tree)
-    ? tree.get("items").size - 1
-    : Object.keys(tree.items).length - 1;
-  let newIds = List<ItemId>([]);
+
+export const getNewItemIds = (tree: TreeData, numIds: number) => {
+  const startNum = Object.keys(tree.items).length - 1;
+  const newIds: ItemId[] = [];
   for (let i = startNum; i < numIds + startNum; i++) {
-    newIds = newIds.push(String(i));
+    newIds.push(String(i));
   }
   return newIds;
-};
-const initTreeItem = {
-  children: [],
-  hasChildren: false,
-  isExpanded: true,
-  isChildrenLoading: false,
-  data: {}
-};
-export const addItemToTree = (tree: TreeData, item: TreeItem) => {
-  return {
-    ...tree,
-    items: {
-      ...tree.items,
-      [item.id]: {
-        ...initTreeItem,
-        ...item
-      }
-    }
-  };
-};
-
-export const getParentItem = (tree: TreeData, childId: ItemId) => {
-  const parentItemEnt = Object.entries(tree.items).filter(ent =>
-    ent[1].children.includes(childId)
-  );
-  return Object.values(Object.fromEntries(parentItemEnt))[0];
-};
-export const removeItemFromTree = (tree: TreeData, itemId: ItemId) => {
-  const parentItem = getParentItem(tree, itemId);
-
-  const newChildren = parentItem.children.filter(id => id !== itemId);
-  return mutateTree(tree, parentItem.id, {
-    children: newChildren,
-    hasChildren: newChildren.length > 0
-  });
-};
-export const combineTwoTimersToSection = (
-  tree: TreeData,
-  source: TreeSourcePosition,
-  destination: TreeDestinationPosition
-) => {
-  const dstItemId = destination.parentId;
-  const srcItemId = tree.items[source.parentId].children[source.index];
-
-  const newSectionId = getNewItemIds(tree, 1).get(0);
-  const newSection = {
-    ...initTreeItem,
-    id: newSectionId,
-    children: [destination.parentId, srcItemId],
-    data: {
-      times: 1
-    }
-  };
-
-  let newTree = removeItemFromTree(tree, srcItemId);
-
-  const dstParentItem = getParentItem(newTree, dstItemId);
-  const newChildren = [...dstParentItem.children].map(id =>
-    id === dstItemId ? newSectionId : id
-  );
-  newTree = addItemToTree(tree, newSection);
-
-  newTree = mutateTree(newTree, dstParentItem.id, { children: newChildren });
-  return newTree;
 };
