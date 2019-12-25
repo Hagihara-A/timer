64a961331b68a6d78d2ae9fff24e37f521f571c7
Hagihara-A <knkr5456@gmail.com>
2019-12-26 01:59:55 +0900

    implemented sample transition between TimerTree and TimerList

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import { createMuiTheme } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/styles";
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { TimerList } from "./components/Timer/TimerList";
 import TimerTree from "./components/Tree/TimerTree";
 import * as serviceWorker from "./serviceWorker";
 import { store } from "./store";
-
+import { useTransition, animated } from "react-spring";
 const theme = createMuiTheme({
   palette: {
     primary: {
@@ -26,15 +26,39 @@ const theme = createMuiTheme({
   }
 });
 
-ReactDOM.render(
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <TimerTree />
-      <TimerList />
-    </ThemeProvider>
-  </Provider>,
-  document.getElementById("root")
-);
+const App = () => {
+  const [isTree, setIsTree] = useState(true);
+  const transitions = useTransition(isTree, null, {
+    from: { opacity: 0, position: "absolute" },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
+  });
+  const Contents = transitions.map(({ item, key, props }) =>
+    item ? (
+      <animated.div style={props}>
+        <TimerTree />
+      </animated.div>
+    ) : (
+      <animated.div style={props}>
+        <TimerList />
+      </animated.div>
+    )
+  );
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        {Contents}
+        <button
+          onClick={() => setIsTree(!isTree)}
+          style={{ position: "relative", top: "500px" }}
+        >
+          TOGGLE
+        </button>
+      </ThemeProvider>
+    </Provider>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
