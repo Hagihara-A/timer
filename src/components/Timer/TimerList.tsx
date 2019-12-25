0b42e53da0d17665d76ee37086eb1b28dc388f90
Hagihara-A <knkr5456@gmail.com>
2019-12-26 01:08:13 +0900

    Changed appearance of TimersList.

--- src/components/Timer/TimerList.tsx
+++ src/components/Timer/TimerList.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { State } from "../../types";
+import styled from "styled-components";
 
+const List = styled.div`
+  text-align: center;
+`;
 export const TimerList = () => {
   const timers = useSelector((state: State) => state.timers);
-  return (
-    <div>
-      {timers.map(item => {
-        return <div key={item.id}>{item.id + "  " + item.data.timeLimit} </div>;
-      })}
-    </div>
-  );
+  const ListItems = timers.map(item => {
+    const { timeLimit, times, power } = item.data;
+    return <li key={item.id}> {`${times} x ${timeLimit} at ${power} W`}</li>;
+  });
+  return <List>{ListItems}</List>;
 };
