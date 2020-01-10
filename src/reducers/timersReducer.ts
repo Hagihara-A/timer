db0a0d81911c0f9ca3d14724ead57eeddcdf99fa
Hagihara-A <knkr5456@gmail.com>
2020-01-10 19:07:27 +0900

    Reducer of FLATTEN_TREE

--- src/reducers/timersReducer.ts
+++ src/reducers/timersReducer.ts
@@ -1,11 +1,15 @@
-import produce from "immer";
+import produce, { Draft } from "immer";
 import { Timers } from "../types";
 import { initState } from "../initState";
 import { Action } from "../types";
-
+import { actionTypes as AT } from "../actions";
+import { flattenTree } from "@atlaskit/tree/dist/cjs/utils/tree";
 export const timersReducer = produce(
-  (timers: Timers = initState.timers, action: Action) => {
+  (timers: Draft<Timers> = initState.timers, action: Action) => {
     switch (action.type) {
+      case AT.FLATTEN_TREE: {
+        return flattenTree(action.payload.sourceTree);
+      }
       default:
         return timers;
     }
