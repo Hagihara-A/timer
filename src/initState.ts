ed64a3be1745df885710f69b09f2fd56bee93d6d
Hagihara-A <knkr5456@gmail.com>
2020-01-10 19:05:34 +0900

    Revised initState

--- src/initState.ts
+++ src/initState.ts
@@ -1,151 +1,7 @@
-import { State } from "./types";
+import { flattenTree } from "@atlaskit/tree/dist/cjs/utils/tree";
+import produce from "immer";
 
-export const initState: State = {
-  timers: [
-    {
-      id: "0",
-      children: [],
-      hasChildren: false,
-      isExpanded: false,
-      isChildrenLoading: false,
-      data: {
-        timeLimit: 3,
-        times: 1,
-        power: 130,
-        comment: ""
-      }
-    },
-    {
-      id: "1",
-      children: ["1-0", "1-1"],
-      hasChildren: true,
-      isExpanded: true,
-      isChildrenLoading: false,
-      data: {
-        timeLimit: 0,
-        times: 1,
-        power: 0,
-        comment: ""
-      }
-    },
-    {
-      id: "1-0",
-      children: [],
-      hasChildren: false,
-      isExpanded: false,
-      isChildrenLoading: false,
-      data: {
-        timeLimit: 2,
-        times: 1,
-        power: 260,
-        comment: ""
-      }
-    },
-    {
-      id: "1-1",
-      children: [],
-      hasChildren: false,
-      isExpanded: false,
-      isChildrenLoading: false,
-      data: {
-        timeLimit: 2,
-        times: 1,
-        power: 260,
-        comment: ""
-      }
-    },
-    {
-      id: "2",
-      children: [],
-      hasChildren: false,
-      isExpanded: false,
-      isChildrenLoading: false,
-      data: {
-        timeLimit: 5,
-        times: 1,
-        power: 260,
-        comment: ""
-      }
-    },
-    {
-      id: "3",
-      children: ["3-0", "3-1", "3-2"],
-      hasChildren: false,
-      isExpanded: true,
-      isChildrenLoading: false,
-      data: {
-        timeLimit: 0,
-        times: 1,
-        power: 0,
-        comment: ""
-      }
-    },
-    {
-      id: "3-0",
-      children: [],
-      hasChildren: false,
-      isExpanded: false,
-      isChildrenLoading: false,
-      data: {
-        timeLimit: 2,
-        times: 1,
-        power: 150,
-        comment: ""
-      }
-    },
-    {
-      id: "3-1",
-      children: [],
-      hasChildren: false,
-      isExpanded: false,
-      isChildrenLoading: false,
-      data: {
-        timeLimit: 2,
-        times: 1,
-        power: 150,
-        comment: ""
-      }
-    },
-    {
-      id: "3-2",
-      children: ["3-2-0", "3-2-1"],
-      hasChildren: true,
-      isExpanded: true,
-      isChildrenLoading: false,
-      data: {
-        timeLimit: 0,
-        times: 1,
-        power: 0,
-        comment: ""
-      }
-    },
-    {
-      id: "3-2-0",
-      children: [],
-      hasChildren: false,
-      isExpanded: false,
-      isChildrenLoading: false,
-      data: {
-        timeLimit: 3,
-        times: 1,
-        power: 150,
-        comment: ""
-      }
-    },
-    {
-      id: "3-2-1",
-      children: [],
-      hasChildren: false,
-      isExpanded: false,
-      isChildrenLoading: false,
-      data: {
-        timeLimit: 3,
-        times: 1,
-        power: 150,
-        comment: ""
-      }
-    }
-  ],
+const state = {
   tree: {
     rootId: "root",
     items: {
@@ -300,5 +156,10 @@ export const initState: State = {
         }
       }
     }
-  }
+  },
+  timers: undefined
 };
+
+export const initState = produce(state, draft => {
+  draft.timers = flattenTree(draft.tree);
+});
