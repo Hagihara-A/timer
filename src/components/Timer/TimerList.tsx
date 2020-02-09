17a3e658cbb9b6e7fbea87bf8559e3724b012d22
Hagihara-A <knkr5456@gmail.com>
2020-02-09 21:59:10 +0900

    More concise way of useSprings

--- src/components/Timer/TimerList.tsx
+++ src/components/Timer/TimerList.tsx
@@ -7,17 +7,24 @@ import { State } from "../../types";
 const AnimatedTyporaphy = animated(Typography);
 
 export const TimerList = () => {
-  const timers = useSelector((state: State) => state.timers);
+  const timers = useSelector((state: State) => state.timers.timerList);
+  const focus = useSelector((state: State) => state.timers.currentTimerIndex);
 
-  const [springs, set] = useSprings(timers.length, () => ({
-    fontSize: "1rem"
-  }));
+  const springs = useSprings(
+    timers.length,
+    timers.map((_, index) => {
+      return {
+        fontSize: index === focus ? "2rem" : "1rem"
+      };
+    })
+  );
+
   const ListItems = springs.map((spring, idx) => {
     const item = timers[idx].item;
-    const { timeLimit, power, times } = item.data;
+    const { timeLimit, power, times, time } = item.data;
     return (
       <AnimatedTyporaphy style={spring} key={item.id}>
-        {`${times} x ${timeLimit} sec at ${power} W`}
+        {`${time}  :: ${times} x ${timeLimit} sec at ${power} W`}
       </AnimatedTyporaphy>
     );
   });
