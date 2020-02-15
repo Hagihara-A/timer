09bee33bf1ad567b776dd945415757664834621c
Hagihara-A <knkr5456@gmail.com>
2020-02-15 20:06:10 +0900

    fix: bug of type guard function

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,13 @@
 import { SectionTreeItemData, TimerTreeItemData, TreeItemData } from "./types";
 
 export const isSection = (data: TreeItemData): data is SectionTreeItemData => {
-  if ("repeat" in data) return true;
+  if ("repeat" in data && "count" in data && !("timeLimit" in data))
+    return true;
   else return false;
 };
 
 export const isTimer = (data: TreeItemData): data is TimerTreeItemData => {
-  if ("elapsedTime" in data) return true;
+  if ("elapsedTime" in data && "power" in data && !("repeat" in data))
+    return true;
   else return false;
 };
