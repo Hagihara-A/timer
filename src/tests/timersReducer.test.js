a439c9e0d3365a6df890dd43506945568c5fa6e9
Hagihara-A <knkr5456@gmail.com>
2020-02-16 12:16:37 +0900

    test: timersReducer

--- src/tests/timersReducer.test.js
+++ src/tests/timersReducer.test.js
@@ -1,11 +1,30 @@
-import { timersReducer } from "../reducers/timersReducer";
 import { actionTypes as AT, addTime } from "../actions";
+import { timersReducer } from "../reducers/timersReducer";
 import { sampleState } from "./testData";
+
 const timers = sampleState.timers;
 
-test(AT.ADD_TIME, () => {
+test(`${AT.ADD_TIME} simply for Timer`, () => {
   const reduced = timersReducer(timers, addTime());
-  expect(timers.timerList[timers.currentTimerIndex].item.data.time + 1).toBe(
-    reduced.timerList[reduced.currentTimerIndex].item.data.time
+  const oldTimerData = timers.timerList[timers.currentTimerIndex].item.data;
+  expect(oldTimerData.elapsedTime + 1).toBe(
+    reduced.timerList[reduced.currentTimerIndex].item.data.elapsedTime
   );
+});
+
+test(`${AT.ADD_TIME} several times, move over Section`, () => {
+  // iterate 5 times
+  let reduced = timers;
+  for (let i = 0; i < 5; i++) {
+    reduced = timersReducer(reduced, addTime());
+  }
+  expect(reduced.timerList[0].item.data.elapsedTime).toBe(
+    reduced.timerList[0].item.data.timeLimit
+  );
+
+  expect(reduced.timerList[1].item.data.count).toBe(1);
+
+  expect(reduced.timerList[2].item.data.elapsedTime).toBe(2);
+
+  expect(reduced.currentTimerIndex).toBe(3);
 });
