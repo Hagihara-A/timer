1d94d787d7a2a2d66b5131c09c0949a1de74d7d3
Hagihara-A <knkr5456@gmail.com>
2020-03-04 23:59:58 +0900

    feat: useFirstTimer's index for initial focus

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -5,6 +5,7 @@ import { initState } from "../initState";
 import { Action, State } from "../types";
 import { timersReducer } from "./timersReducer";
 import { treeReducer } from "./treeReducer";
+import { isTimer } from "../utils";
 
 // Child reducer should return draft state passed from parent reducer.
 // It doen't make a difference between `return draft` and `break`.Former is preferred.
@@ -12,6 +13,9 @@ export const rootReducer = produce((draft: Draft<State>, action: Action) => {
   switch (action.type) {
     case AT.FLATTEN_TREE: {
       draft.timers.timerList = flattenTree(draft.tree);
+      draft.timers.currentTimerIndex = draft.timers.timerList.findIndex(elem =>
+        isTimer(elem.item.data)
+      );
       break;
     }
     default: {
