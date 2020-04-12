92eac7fc63e6e301128a08c1a0ef230f30476602
Hagihara-A <knkr5456@gmail.com>
2020-04-12 21:38:17 +0900

    test: test case for resetDescendant & ADD_TIME 40, 50 times

--- src/tests/timersReducer.test.ts
+++ src/tests/timersReducer.test.ts
@@ -1,10 +1,18 @@
+import { ItemId } from "@atlaskit/tree";
 import produce from "immer";
 import { actionTypes as AT, addTime } from "../actions";
 import { initState } from "../initState";
-import { countUp, timersReducer, traverse } from "../reducers/timersReducer";
-import { TreeItem } from "../types";
+import {
+  countUp,
+  timersReducer,
+  traverse,
+  resetDescendant
+} from "../reducers/timersReducer";
+import { TreeData, TreeItem } from "../types";
 import { isTimer } from "../utils";
+
 const { timers } = initState;
+const { timerTree } = timers;
 
 declare global {
   namespace jest {
@@ -13,6 +21,7 @@ declare global {
     }
   }
 }
+
 expect.extend({
   toBeCountedTo(received: TreeItem, count: number | "max") {
     let pass: boolean;
@@ -35,11 +44,11 @@ expect.extend({
     } else {
       if (isTimer(received.data)) {
         actual = received.data.elapsedTime;
-        pass = actual === count;
       } else {
         actual = received.data.count;
-        pass = actual === count;
       }
+      pass = actual === count;
+
       return {
         pass,
         message: () => `expected ${count} but got ${actual}`
@@ -47,44 +56,44 @@ expect.extend({
     }
   }
 });
-test(`${AT.ADD_TIME} simply for Timer`, () => {
-  const oldTimerData = timers.timerList[timers.currentTimerId].item.data;
-  const reduced = timersReducer(timers, addTime());
-  const newTimerData = reduced.timerList[reduced.currentTimerId].item.data;
-  expect(newTimerData.elapsedTime).toBe(oldTimerData.elapsedTime + 1);
-});
 
-test(`${AT.ADD_TIME} 10 times, step over Section`, () => {
-  // iterate 10 times
-  let reduced = timers;
-  for (let i = 0; i < 10; i++) {
-    reduced = timersReducer(reduced, addTime());
-  }
-  expect(reduced.timerTree.items["0"].data.elapsedTime).toBe(3);
-  expect(reduced.timerList[1].item.data.count).toBe(1);
-
-  expect(reduced.timerList[2].item.data.elapsedTime).toBe(2);
-  expect(reduced.timerList[3].item.data.elapsedTime).toBe(2);
-  expect(reduced.timerList[4].item.data.elapsedTime).toBe(3);
-
-  expect(reduced.currentTimerId).toBe(4);
-});
-
-const times = 20;
-test(`${AT.ADD_TIME} ${times} times, start from path:[3, 0]`, () => {
-  //` iterate ${times} times`;
-  let reduced = produce(timers, draft => {
-    draft.currentTimerId = 6;
+describe(`${AT.ADD_TIME}`, () => {
+  test(`${AT.ADD_TIME} simply for Timer`, () => {
+    const reduced = timersReducer(timers, addTime());
+    expect(reduced.timerTree.items["0"]).toBeCountedTo(1);
   });
-  for (let i = 0; i < times; i++) {
-    reduced = timersReducer(reduced, addTime());
-  }
-  expect(reduced.timerList[6].item.data.elapsedTime).toBe(2);
-  expect(reduced.timerList[7].item.data.elapsedTime).toBe(2);
 
-  expect(reduced.timerList[8].item.data.count).toBe(2);
-  expect(reduced.timerList[9].item.data.elapsedTime).toBe(3);
-  expect(reduced.timerList[10].item.data.elapsedTime).toBe(1);
+  test(`${AT.ADD_TIME} 40 times`, () => {
+    // iterate 40 times
+    let reduced = timers;
+    for (let i = 0; i < 40; i++) {
+      reduced = timersReducer(reduced, addTime());
+    }
+    const reducedItems = reduced.timerTree.items;
+
+    expect(reducedItems["0"]).toBeCountedTo("max");
+    expect(reducedItems["1"]).toBeCountedTo("max");
+    expect(reducedItems["1-0"]).toBeCountedTo("max");
+    expect(reducedItems["3"]).toBeCountedTo(1);
+    expect(reducedItems["3-2"]).toBeCountedTo(1);
+    expect(reducedItems["3-2-0"]).toBeCountedTo(2);
+    expect(reducedItems["3-2-1"]).toBeCountedTo(0);
+  });
+
+  test(`${AT.ADD_TIME} 50 times, should be ended`, () => {
+    //` iterate 50 times`;
+    let reduced = timers;
+    for (let i = 0; i < 50; i++) {
+      reduced = timersReducer(reduced, addTime());
+    }
+    const reducedItems = reduced.timerTree.items;
+    expect(reducedItems["0"]).toBeCountedTo("max");
+    expect(reducedItems["1"]).toBeCountedTo("max");
+    expect(reducedItems["1-0"]).toBeCountedTo("max");
+    expect(reducedItems["3"]).toBeCountedTo("max");
+    expect(reducedItems["3-2-1"]).toBeCountedTo("max");
+    expect(reducedItems["3-2"]).toBeCountedTo("max");
+  });
 });
 
 describe(`traverse`, () => {
@@ -138,7 +147,38 @@ describe(`traverse`, () => {
 
 describe(`countUp`, () => {
   test(`countUp initState`, () => {
-    const counted = produce(timers, draft => countUp(draft));
-    expect(counted.timerTree.items["0"]).toBeCountedTo(1);
+    const counted = produce(timers.timerTree, draft => countUp(draft));
+    expect(counted.items["0"]).toBeCountedTo(1);
   });
+});
+
+const elapse = (tree: TreeData, ids: ItemId[], count?: number) => {
+  for (const id of ids) {
+    const item = tree.items[id];
+    if (typeof count === "undefined") {
+      if (isTimer(item.data)) item.data.elapsedTime = item.data.timeLimit;
+      else item.data.count = item.data.repeat;
+    } else {
+      if (isTimer(item.data)) item.data.elapsedTime = count;
+      else item.data.count = count;
+    }
+  }
+};
+test(`resetDescendant`, () => {
+  const elapsedTree = produce(timerTree, draft => {
+    elapse(
+      draft,
+      Object.keys(timerTree.items).filter(id => id !== timerTree.rootId)
+    );
+  });
+
+  const resetUnder3 = produce(elapsedTree, draft => {
+    resetDescendant(draft, "3");
+  });
+
+  expect(resetUnder3.items["1-0"]).toBeCountedTo("max");
+  expect(resetUnder3.items["3"]).toBeCountedTo("max");
+  expect(resetUnder3.items["3-1"]).toBeCountedTo(0);
+  expect(resetUnder3.items["3-2"]).toBeCountedTo(0);
+  expect(resetUnder3.items["3-2-1"]).toBeCountedTo(0);
 });
