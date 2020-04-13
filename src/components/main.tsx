5edae548e5d1f341ca6dddd7b619d1baa3107a20
Hagihara-A <knkr5456@gmail.com>
2020-04-13 22:55:50 +0900

    feat: show/hide buttons depending on isDragEnabled

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,6 +1,6 @@
 import { Typography } from "@material-ui/core";
 import React, { useRef, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   addTime,
   addTimer as addTimerAct,
@@ -11,8 +11,12 @@ import { AddTimerDialog } from "./AddTimerDialog";
 import { TimerListIcons } from "./Timer/TimerListButtons";
 import TimerTree from "./Tree/TimerTree";
 import { TimerTreeIcons } from "./Tree/TimerTreeButtons";
+import { State } from "../types";
 
 const TimerApp = () => {
+  const isDragEnabled = useSelector(
+    (state: State) => state.options.isDragEnabled
+  );
   const [isModalOpen, setIsModalOpen] = useState(false);
   const toggleIsModalOpen = () => setIsModalOpen(!isModalOpen);
   const dispatch = useDispatch();
@@ -54,15 +58,18 @@ const TimerApp = () => {
         Training Timer
       </Typography>
       <TimerTree />
-      <TimerTreeIcons
-        onClickAdd={toggleIsModalOpen}
-        onClickComplete={toggleIsDragEnabled}
-      />
-      <TimerListIcons
-        onClickStart={startTimerDispatch}
-        onClickStop={stopTimerDispatch}
-        onClickReset={resetTimerDispatch}
-      />
+      {isDragEnabled ? (
+        <TimerTreeIcons
+          onClickAdd={toggleIsModalOpen}
+          onClickComplete={toggleIsDragEnabled}
+        />
+      ) : (
+        <TimerListIcons
+          onClickStart={startTimerDispatch}
+          onClickStop={stopTimerDispatch}
+          onClickReset={resetTimerDispatch}
+        />
+      )}
       <AddTimerDialog
         open={isModalOpen}
         onClose={toggleIsModalOpen}
