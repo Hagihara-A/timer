e744338e69f7720f01db60e0ee516263a1441181
Hagihara-A <knkr5456@gmail.com>
2020-04-20 19:24:58 +0900

    feat: display AppBar

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,20 +1,39 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Login } from "./Login";
 import { TimerApp } from "./TimerApp";
-import { Typography } from "@material-ui/core";
+import { Typography, AppBar, Toolbar, Button } from "@material-ui/core";
+import styled from "styled-components";
+
+const RightButton = styled(Button)`
+  margin-left: auto;
+`;
+const Bar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" align="left">
+          Training Timer
+        </Typography>
+        <RightButton variant="outlined"> Login</RightButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
 export const Main = () => {
   return (
     <BrowserRouter>
-      <Typography variant="h1" paragraph align="center">
-        Training Timer
-      </Typography>
+      <Bar />
+      <div style={{ height: "10px" }}></div>
       <Switch>
         <Route path="/login">
           <Login />
         </Route>
-        <Route path="/">
+        <Route exact path="/">
           <TimerApp />
+        </Route>
+        <Route path="/">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </BrowserRouter>
