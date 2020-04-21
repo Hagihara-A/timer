b78492e4ce50d95e032cebd2027a535b07ae9857
Hagihara-A <knkr5456@gmail.com>
2020-04-21 17:29:10 +0900

    feat: Login/Log out button

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,6 @@
 import { AppBar, Button, Link, Toolbar } from "@material-ui/core";
-import React from "react";
+import React, { useEffect } from "react";
+import * as firebase from "firebase";
 import {
   BrowserRouter,
   Redirect,
@@ -10,27 +11,47 @@ import {
 import styled from "styled-components";
 import { Login } from "./Login";
 import { TimerApp } from "./TimerApp";
+import { useAppState } from "../utils";
+import { useDispatch } from "react-redux";
+import { setUser } from "../actions";
 
 const RightButton = styled(Button)`
   margin-left: auto;
 `;
 const Bar = () => {
   const hist = useHistory();
+  const user = useAppState((state) => state.options.user);
   return (
     <AppBar position="static">
       <Toolbar>
         <Link href="/" color="initial" underline="none" variant="h6">
           Training Timer
         </Link>
-        <RightButton variant="outlined" onClick={() => hist.push("/login")}>
-          {" "}
-          Login
-        </RightButton>
+        {user ? (
+          <RightButton
+            variant="outlined"
+            onClick={() => firebase.auth().signOut()}
+          >
+            {"log out"}
+          </RightButton>
+        ) : (
+          <RightButton variant="outlined" onClick={() => hist.push("/login")}>
+            {"log in"}
+          </RightButton>
+        )}
       </Toolbar>
     </AppBar>
   );
 };
 export const Main = () => {
+  const dispatch = useDispatch();
+  const user = useAppState((state) => state.options.user);
+  useEffect(() => {
+    const unsubscribeAuth = firebase.auth().onAuthStateChanged((newUser) => {
+      dispatch(setUser(newUser));
+    });
+    return unsubscribeAuth;
+  }, [user, dispatch]);
   return (
     <BrowserRouter>
       <Bar />
